128b761ca58bd44f0034704a3cf460f70f90b86d
Yurez123 <75393299+Yurez123@users.noreply.github.com>
2024-04-21 11:54:16 +0300

    vine

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+var ageDima = 12;
+let age = 12;
+let text = "I'am a string";
+let is_activ = true;
+const age_1 = 20;
+
+// let age = 16;
+// var age_2 = 16;
+
+var my_name = " Yurii ";
+let lastName = "Dart";
+const work = "developer";
+let my_age = 25;
+
+let about_me = `My name is ${my_name}. I'm will be ${work}.`
+
+console.log(my_name.length)
+console.log(about_me)
+
+
+$('.slider').slick({
+    arrows: false,
+    dots: true,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
